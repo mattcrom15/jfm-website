876c0107b4e9d01edbe4cbf971b7356f28d0f31a
Matt Cromwell <66025680+mattcrom15@users.noreply.github.com>
2020-09-30 22:09:24 +0100

    Mobile (#5)
    * fixed slideshow
    
    * changed logo classname
    
    * home basic mobile styles
    
    * mobile shop styles
    
    * about page styles
    
    * product styles
    
    * corrected about info style
    
    * working through production style
    
    * menu icons
    
    * removed console logs
    
    * add icons
    
    * new menu styles
    
    * fixed front page
    
    * added hamburger menu

--- header.php
+++ header.php
@@ -9,8 +9,9 @@
 </head>
 <body>
 <header>
-<a href="<?php echo get_home_url();?>"><img class="logo-placeholder" src="http://jfm-website.test/wp-content/uploads/2020/09/jfm-logo_240920.png" alt="" srcset=""></a>
-<nav>
+<a href="<?php echo get_home_url();?>"><img class="logo" src="http://jfm-website.test/wp-content/uploads/2020/09/jfm-logo_240920.png" alt="" srcset=""></a>
+<img src="http://jfm-website.test/wp-content/themes/JFM-Theme-2020/images/menu.svg" alt="" class="hamburger" onclick="MobileMenu();"> 
+<nav id="nav-menu" status="closed">
             <?php 
         wp_nav_menu(
             array(
@@ -18,6 +19,7 @@
             )
         );
         ?>
+        <img src="http://jfm-website.test/wp-content/themes/JFM-Theme-2020/images/x.svg" alt="" class="close-ico" onclick="MobileMenu();">
       </nav>
 
 </header>
--- template-about.php
+++ template-about.php
@@ -6,10 +6,10 @@ Template Name: about
 
 <?php get_header('About');?>
 
-<div style="display: flex; padding: 5rem">
-      <img src="<?php the_field('about_image')?>" alt="" srcset="">
+<div class="about-content">
+      <img src="<?php the_field('about_image')?>" alt="" srcset="" class="about-image">
       <div class="dotted-line"></div>
-      <div class="about-info">
+      <div class="about-info-page">
         <h3 style="margin-bottom: 0.7rem">About</h3>
         <p>
           <?php the_field('about_information')?>
