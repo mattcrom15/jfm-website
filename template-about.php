6dda1f840cb40db2dc9faa459ffe69ecded18b3e
Matt Cromwell <66025680+mattcrom15@users.noreply.github.com>
2020-09-30 17:45:53 +0100

    corrected about info style

--- template-about.php
+++ template-about.php
@@ -9,7 +9,7 @@ Template Name: about
 <div style="about-content">
       <img src="<?php the_field('about_image')?>" alt="" srcset="" class="about-image">
       <div class="dotted-line"></div>
-      <div class="about-info">
+      <div class="about-info-page">
         <h3 style="margin-bottom: 0.7rem">About</h3>
         <p>
           <?php the_field('about_information')?>
