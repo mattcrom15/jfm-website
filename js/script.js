2e774ddde796293bb6f3f9cef615a704bc728555
Matt C <matt@markcunniffe.com>
2020-09-30 15:24:36 +0100

    added image slideshow function

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
-function GetProduct(elem){
-    window.location = window.origin + '/product'
+
+function Slideshow(elem){
+    const images = document.querySelectorAll('.product-hero-image');
+    const thumbid = elem.getAttribute('thumbnailid');
+
+    for (let t = 0; t < images.length; t++) {
+        const element = images[t];
+        if (element.getAttribute('photoid') == thumbid){
+            console.log('same image');
+            element.style.opacity = 1;
+            
+        }else{
+            console.log('different image');
+            element.style.opacity = 0;
+            
+        }
+        
+    }
+
 }
