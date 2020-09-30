8d2a4cf11c814b1a1c5d12e1403416214147c536
Matt Cromwell <66025680+mattcrom15@users.noreply.github.com>
2020-09-30 15:31:45 +0100

    Revert "Slideshow"

--- js/script.js
+++ js/script.js
@@ -1,20 +1,3 @@
-
-function Slideshow(elem){
-    const images = document.querySelectorAll('.product-hero-image');
-    const thumbid = elem.getAttribute('thumbnailid');
-
-    for (let t = 0; t < images.length; t++) {
-        const element = images[t];
-        if (element.getAttribute('photoid') == thumbid){
-            console.log('same image');
-            element.style.opacity = 1;
-            
-        }else{
-            console.log('different image');
-            element.style.opacity = 0;
-            
-        }
-        
-    }
-
+function GetProduct(elem){
+    window.location = window.origin + '/product'
 }
