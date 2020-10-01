7fe40dae5c0d4d85894ac740febc10ee9b464c54
Matt Cromwell <66025680+mattcrom15@users.noreply.github.com>
2020-10-01 19:02:23 +0100

    Update 404.php

--- 404.php
+++ 404.php
@@ -1,5 +1,4 @@
 <?php get_header('Product');?>
 
 <h1>404</h1>
-<h1>you’re in the wrong neighbourhood brother</h1>
-<img src="http://jfm-website.test/wp-content/uploads/2020/09/118289511_328208921709917_288487491002189558_n.jpg" alt="" srcset="" style="width:50%;">
+<p>The page you are looking for doesn't exist!</p>
